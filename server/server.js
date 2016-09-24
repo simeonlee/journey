09662a714a48e0890af4ec94ab3b4b2e7ba194ac
Connor Chevli <connorch@buffalo.edu>
2016-09-23 17:34:30 -0700

    Fix syntax error.  Add npm start command

--- server/server.js
+++ server/server.js
@@ -11,6 +11,6 @@ require('./config/routes.js')(app, express);
 //start listening to requests on port 3000.
 app.listen(3000, () => {
   console.log('Listening on Port: 3000');
-};
+});
 
 module.exports = app;
