328fb8b77117e4f9bfaf56d09186a398145abe3e
Akai Senghor <wasterni@gmail.com>
2016-09-24 15:45:06 -0700

    Add start of nav bar

---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavLink from './NavLink'
+
+exports class Navbar extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
---
+++ components/app.jsx
@@ -0,0 +1,20 @@
+import { Component } from React.Component;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        
+      </div>
+    )
+  }
+}
