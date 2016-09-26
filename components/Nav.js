b8b5e68e4cd7abc77cac347886e1affbcfaa5fbd
Akai Senghor <wasterni@gmail.com>
2016-09-26 12:04:13 -0700

    Add style for nav

--- components/Nav.js
+++ components/Nav.js
@@ -13,14 +13,14 @@ export class Nav extends Component {
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-format">
               <li className="navbar-links"><Link activeClassName="nav-active" to="/journal">Journal</Link></li>
-              <li className="navbar-links"><Link to="/dashboard">Dashboard</Link></li>
+              <li className="navbar-links"><Link activeClassName="nav-active" to="/dashboard">Dashboard</Link></li>
             </ul>
             <div className="navbar-header custom-header">
               <IndexLink to="/" className="navbar-brand">Journey</IndexLink>
             </div>
             <ul className="nav navbar-nav navbar-right second navbar-format margin-right">
-              <li className="navbar-links"><Link to="/profile">Profile</Link></li>
-              <li className="navbar-links"><Link to="/signin">Sign In</Link></li>
+              <li className="navbar-links"><Link activeClassName="nav-active" to="/profile">Profile</Link></li>
+              <li className="navbar-links"><Link activeClassName="nav-active" to="/signin">Sign In</Link></li>
             </ul>
           </div>
         </div>
