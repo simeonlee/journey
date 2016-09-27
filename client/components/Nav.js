65bc9ef5f40bce8952babdfd95481e7d27f13be5
Simeon Lee <simeonhlee@gmail.com>
2016-09-27 11:28:57 -0700

    Refactor and update formatting of navbar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -8,27 +8,27 @@ export class Nav extends Component {
 
   render() {
     return (
-      <nav className="transparent haze wrap navbar navbar-default" role="navigation">
+      <nav className="transparent haze-background wrap navbar navbar-default" role="navigation">
         <div className="container-fluid">
           <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+            <button type="button" className="haze-background navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
           </div>
-          <div className="collapse navbar-collapse" id="navigation">
-            <ul className="nav navbar-nav navbar-format">
-              <li className="navbar-links"><Link activeClassName="nav-active" to="/journal">Journal</Link></li>
-              <li className="navbar-links"><Link activeClassName="nav-active" to="/dashboard">Dashboard</Link></li>
+          <div className="brand-centered">
+            <IndexLink to="/" className="navbar-brand">Journey</IndexLink>
+          </div>
+          <div className="navbar-collapse collapse" id="navigation">
+            <ul className="nav navbar-nav navbar-left">
+              <li><Link activeClassName="nav-active" to="/journal">Journal</Link></li>
+              <li><Link activeClassName="nav-active" to="/dashboard">Dashboard</Link></li>
             </ul>
-            <div className="navbar-header custom-header">
-              <IndexLink to="/" className="navbar-brand">Journey</IndexLink>
-            </div>
-            <ul className="nav navbar-nav navbar-right second navbar-format">
-              <li className="navbar-links"><Link activeClassName="nav-active" to="/profile">Profile</Link></li>
-              <li className="navbar-links"><Link activeClassName="nav-active" to="/signin">Log In</Link></li>
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link activeClassName="nav-active" to="/profile">Profile</Link></li>
+              <li><Link activeClassName="nav-active" to="/signin">Log In</Link></li>
             </ul>
           </div>
         </div>
