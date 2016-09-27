420b9c0c54445bcdec5079792a551d9508174216
Connor Chevli <connorch@buffalo.edu>
2016-09-27 15:58:57 -0700

    Add facebook login button (functional.. ish)

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FacebookLogin } from 'react-facebook-login-component';
 
 export class SignIn extends Component {
-  constructor(props) {
-    super(props)
+  constructor(props, context) {
+    super(props, context);
   }
+
+  responseFacebook (response, a) {
+    console.log(response, a);
+    //anything else you want to do(save to localStorage)...
+  }
+
   render() {
     return (
       <div>
-        <div>
-          What up
-        </div>
+        <FacebookLogin socialId="375795929474685"
+                       language="en_US"
+                       scope="public_profile,email"
+                       responseHandler={this.responseFacebook}
+                       xfbml={true}
+                       version="v2.5"
+                       class="facebook-login"
+                       buttonText="Login With Facebook"/>
       </div>
     )
   }
