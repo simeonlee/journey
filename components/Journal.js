d721ea5929f79d3e949fda31367bc14c50125165
Akai Senghor <wasterni@gmail.com>
2016-09-27 09:49:26 -0700

    Changes

--- components/Journal.js
+++ components/Journal.js
@@ -6,7 +6,26 @@ export class Journal extends Component {
   }
   render() {
     return (
-      <div className="journal">Journal yo</div>
+      <div>
+        <div className="col-md-12">
+          <ul className="timeline">
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+            <li className="timeline-date"><a href="" className="activeClassName">testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting</a></li>
+          </ul>
+        </div>
+        <div className="col-md-offset-2 col-md-8">
+          <div className="">
+            
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
--- components/Profile.js
+++ components/Profile.js
@@ -6,9 +6,22 @@ export class Profile extends Component {
   }
   render() {
     return (
-      <div>
-        <div>
+      <div className="col-md-offset-2 col-md-8">
+        <h2>
           Profile
+        </h2>
+        <div>
+          <ul className="ul-profile col-md-3">
+            <li className="profile-list"><a href="settings" className="activeClassName">Settings</a></li>
+            <li className="profile-list"><a href="personal-info" className="activeClassName">Personal Info</a></li>
+            <li className="profile-list"><a href="" className="activeClassName">Whatever</a></li>
+            <li className="profile-list"><a href="" className="activeClassName">We</a></li>
+            <li className="profile-list"><a href="" className="activeClassName">Need</a></li>
+            <li className="profile-list"><a href="" className="activeClassName">Yo</a></li>
+          </ul>
+        </div>
+        <div className="col-md-9">
+          yoyo
         </div>
       </div>
     )
