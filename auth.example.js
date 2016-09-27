37014582041b3383b77102af9a61620217a08701
Connor Chevli <connorch@buffalo.edu>
2016-09-26 17:30:05 -0700

    Start adding Oauth capabilities

--- auth.example.js
+++ auth.example.js
@@ -1,6 +1,7 @@
 module.exports = {
   facebook: {
     FACEBOOK_APP_ID: 'FACEBOOK_APP_ID',
-    FACEBOOK_APP_SECRET: 'FACEBOOK_APP_SECRET'
+    FACEBOOK_APP_SECRET: 'FACEBOOK_APP_SECRET',
+    callbackUrl: 'http://localhost:8080/auth/facebook/callback'
   }
 }
