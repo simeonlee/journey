4f4c32b4bfd91b8a8ffef555d8c501ee60896623
Simeon Lee <simeonhlee@gmail.com>
2016-09-27 10:33:47 -0700

    Update stylings on nav bar and css

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -8,9 +8,17 @@ export class Nav extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-default">
+      <nav className="transparent haze wrap navbar navbar-default" role="navigation">
         <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+          </div>
+          <div className="collapse navbar-collapse" id="navigation">
             <ul className="nav navbar-nav navbar-format">
               <li className="navbar-links"><Link activeClassName="nav-active" to="/journal">Journal</Link></li>
               <li className="navbar-links"><Link activeClassName="nav-active" to="/dashboard">Dashboard</Link></li>
@@ -20,7 +28,7 @@ export class Nav extends Component {
             </div>
             <ul className="nav navbar-nav navbar-right second navbar-format">
               <li className="navbar-links"><Link activeClassName="nav-active" to="/profile">Profile</Link></li>
-              <li className="navbar-links"><Link activeClassName="nav-active" to="/signin">Sign In</Link></li>
+              <li className="navbar-links"><Link activeClassName="nav-active" to="/signin">Log In</Link></li>
             </ul>
           </div>
         </div>
