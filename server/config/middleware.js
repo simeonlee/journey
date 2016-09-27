0f8c7da0c35c795a0534c6863daeb7c9b72f2520
Connor Chevli <connorch@buffalo.edu>
2016-09-27 15:57:57 -0700

    update middleware to use passport.  start setup for authentication routes

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,13 +2,25 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
 var favicon = require('serve-favicon');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
-module.exports = (app, express) => {
+module.exports = (app, express, passport) => {
   app.use(morgan('dev'));
+
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '../../', 'dist')));
   app.use(favicon(path.join(__dirname, '../../', 'dist', 'images', 'icons', 'favicon', 'favicon.ico')));
+
+  app.use(cookieParser());
+  app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: false,
+  }))
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // The '/scripts' endpoint below serves up 'node_modules' buried in the
   // root directory which is inaccessible by index.html from /client
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,2 +1,20 @@
+var passport = require('passport');
+
 module.exports = (app, express) => {
+  app.post('/login/facebook',
+    passport.authenticate('facebook', { successRedirect: '/login/facebook/callback',
+                                        failureRedirect: '/dashboard' }),
+    (req, res) => {
+      console.log('POST REQ TO LOGIN/FACEBOOK!!!')
+      res.send();
+    }
+  );
+
+  // handle the callback after facebook has authenticated the user
+  app.get('/login/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect : '/home',
+      failureRedirect : '/'
+    })
+  );
 }
