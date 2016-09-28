ce9224724cce70141e1d82d9ea875908a1175344
Simeon Lee <simeonhlee@gmail.com>
2016-09-27 19:47:20 -0700

    Update for new analytics dashboard

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
+import Affirmations from './analytics/Affirmations'
 
-export class Dashboard extends Component {
+export default class Dashboard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      data: {
+        url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/98887/d3pack-flare-short.json',
+        elementDelay: 100
+      },
+      startDelay: 2000
+    };
+  }
+  componentDidMount() {
+    console.log('rendering');
   }
   render() {
+    var style = {
+      width: '100%',
+      height: '100%'
+    };
     return (
-      <div>
-        <div>
-          Dashboard
-        </div>
+      <div style={style}>
+        <Affirmations
+          startDelay={this.state.startDelay}
+          elementDelay={this.state.data.elementDelay}
+          json={this.state.data.url}
+        />
       </div>
     )
   }
