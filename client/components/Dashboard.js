ba555027b6ce0815cc3960d31d1bd56fbf8c04a3
Simeon Lee <simeonhlee@gmail.com>
2016-09-28 15:00:57 -0700

    Update for circles and new sample data

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import Affirmations from './analytics/Affirmations'
+import Circles from './analytics/Circles'
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {
-        url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/98887/d3pack-flare-short.json',
+        url: './data/sample.json',
         elementDelay: 100
       },
       startDelay: 2000
@@ -16,13 +16,9 @@ export default class Dashboard extends Component {
     console.log('rendering');
   }
   render() {
-    var style = {
-      width: '100%',
-      height: '100%'
-    };
     return (
-      <div style={style}>
-        <Affirmations
+      <div className="dashboard-container">
+        <Circles
           startDelay={this.state.startDelay}
           elementDelay={this.state.data.elementDelay}
           json={this.state.data.url}
