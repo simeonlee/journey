ac47f900f6c38c8a03015d0048e8fb194e9cd38a
Simeon Lee <simeonhlee@gmail.com>
2016-09-29 19:45:06 -0700

    Instantiate timeline component

---
+++ client/components/analytics/Timeline.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="timeline">
+        <div></div>
+      </div>
+    )
+  }
+}
