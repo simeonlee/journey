5760ca9c37e8aee18274a95f9f2fb68b736bf713
Connor Chevli <connorch@buffalo.edu>
2016-10-05 10:52:47 -0700

    On facebook login, users now create a user model and facebook model appropriately (if one doesn't already exist)

--- server/config/utils.js
+++ server/config/utils.js
@@ -3,8 +3,6 @@ var passport = require('passport');
 module.exports = function() {
 
   var checkForFacebookUser = (req, res) => {
-    console.log('checking for user')
-    console.log('AUTHENTICATED: ', req.isAuthenticated());
     res.send(req.isAuthenticated());
   }
 
@@ -30,21 +28,47 @@ module.exports = function() {
         lastLoginDate: Date.now(),
         createdAt: Date.now()
       }
+    }).spread((user, created) => {
+      console.log('user!!!!!', user);
+      FacebookUser.findOrCreate({ where: { facebookID: profile.id },
+        defaults: {
+          facebookID: profile.id,
+          userId: user.id
+        }
+      }).spread((user, created) => {
+        return done(null, user);
+      });
     })
+  }
 
-    FacebookUser.findOrCreate({
-      where: {facebookID: profile.id},
-      defaults: {
-        facebookID: profile.id,
-        userId: 1
-      }
-    }).spread((user, wasCreated) => {
-      return done(null, user);
-    });
+  var findOrCreateAmazonUser = (User, AmazonUser, profile, done) => {
+    // User.findOrCreate({
+    //   where: {
+    //     username: profile.email
+    //   },
+    //   defaults: {
+    //     password: '12345',
+    //     email: profile.email,
+    //     // phone: '716-472-9022',
+    //     firstName: profile.name.givenName,
+    //     lastName: profile.name.familyName,
+    //     age: 23,
+    //     gender: profile.gender,
+    //     // bio: ,
+    //     // job: 'fulltime avocado',
+    //     // industry: 'avacadoing',
+    //     // employer: 'lettuce',
+    //     wantsEmails: 1,
+    //     wantsTexts: 0,
+    //     lastLoginDate: Date.now(),
+    //     createdAt: Date.now()
+    //   }
+    // })
   }
 
   return {
     checkForFacebookUser: checkForFacebookUser,
-    findOrCreateFbUser: findOrCreateFbUser
+    findOrCreateFbUser: findOrCreateFbUser,
+    findOrCreateAmazonUser: findOrCreateAmazonUser
   }
 }()
