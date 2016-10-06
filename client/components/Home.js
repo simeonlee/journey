182317b6afd45cf160577db477937a9b99855779
Connor Chevli <connorch@buffalo.edu>
2016-10-06 15:07:34 -0700

    add images for home

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Col, Grid, Row, Button, FieldGroup } from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
 
 export class Home extends Component {
   constructor(props) {
@@ -6,22 +8,48 @@ export class Home extends Component {
   }
   render() {
     return (
-      <div className="homepage-background">
-        <div className="home-top-content-container col-md-10 col-md-offset-1">
-          <div>
-            <div className="col-md-6">
-              <h1>
-                Home
-              </h1>
-            </div>
-            <div className="col-md-6">
-              <h1>
-                Test
-              </h1>
-            </div>
-          </div>
-        </div>
+      <div>
+        
+        <Jumbotron className="home-jumbotron">
+          <h2 className="col-md-offset-1 col-md-4 jumbo-left-text">Your journey starts here.</h2>
+          <p className="col-md-offset-1">Release your thoughts and </p>
+          <p>lakjsdlfj;sdlkjal</p>
+        </Jumbotron>
+        <Jumbotron className="home-jumbotron">
+          <p className="col-md-offset-1 col-md-7">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <h1 className="col-md-offset-1 jumbo-right-text">Sign Up!</h1>
+        </Jumbotron>
+        <Jumbotron className="home-jumbotron">
+          <h2 className="col-md-offset-1 col-md-4 left-jumbo-1">Your journey starts here.</h2>
+          <p className="col-md-offset-1">Write, Talk, Text - Journaling made easy.</p>
+          <p>lakjsdlfj;sdlkjal</p>
+        </Jumbotron>
+        <Jumbotron className="home-jumbotron">
+          <p className="col-md-offset-1 col-md-7">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <h1 className="col-md-offset-1 jumbo-right-text">Journey</h1>
+        </Jumbotron>
       </div>
     )
+
+    // return (
+    //   <div>
+    //     <div id="homepage-background col-md-12">
+    //       <div className="home-top-content-container col-md-10 col-md-offset-1">
+    //         <div>
+    //           <div className="col-md-6">
+    //             <h1>Home</h1>
+    //           </div>
+    //           <div className="col-md-6">
+    //             <h1>Sign Up</h1>
+    //             <p>
+    //               It's Free!
+    //             </p>
+    //             <SignUp/>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
   }
 }
