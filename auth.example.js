1ba988f827a3be2ad4d33301d5ae170cffcd27ad
Simeon Lee <simeonhlee@gmail.com>
2016-10-08 11:49:26 -0700

    Add template for google cloud api keys

--- auth.example.js
+++ auth.example.js
@@ -9,5 +9,17 @@ module.exports = {
     appID: 'AMAZON_APP_ID',
     appSecret: 'AMAZON_APP_SECRET',
     callbackUrl: 'http://127.0.0.1:3000/auth/amazon/callback'
+  },
+  google: {
+    type: 'service_account',
+    project_id: 'GOOGLE_PROJECT_ID',
+    private_key_id: 'GOOGLE_PRIVATE_KEY_ID',
+    private_key: 'GOOGLE_PRIVATE_KEY',
+    client_email: 'GOOGLE_CLIENT_EMAIL',
+    client_id: 'GOOGLE_CLIENT_ID',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://accounts.google.com/o/oauth2/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: 'GOOGLE_CLIENT_x509_CERT_URL'
   }
 }
