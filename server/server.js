f7e8a9699ad568344154b78abccc6ec916429ab8
Connor Chevli <connorch@buffalo.edu>
2016-10-11 19:08:21 -0700

    update server.js for deployed version

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
 var { sequelize } = require('./db/config');
 var port = 5000;
 
@@ -29,7 +33,27 @@ require('./config/passport.js')(passport); // Authentication
 require('./config/middleware.js')(app, express, passport); // Middleware
 require('./config/routes.js')(app); // Routes
 
+
+// var privateKey = fs.readFileSync('./https/www_yourjourney_io.key');
+// var certificate = fs.readFileSync('./https/www_yourjourney_io.crt');
+// var pemBundle = fs.readFileSync('./https/www_yourjourney_io_bundle.pem');
+
+// serverOptions = {
+//   ca: pemBundle,
+//   key: privateKey,
+//   cert: certificate
+// }
+
+// sequelize.sync().then(function() {
+//   console.log('Synced with mySQL through Sequelize.');
+
+//   https.createServer(serverOptions, app).listen(3000);
+//   http.createServer(app).listen(1337);
+
+//   console.log('Listening on Port: 3000');
+// });
 // Start server
+
 app.listen(port, () => {
   console.log('Listening on Port: ' + port);
   sequelize.sync().then(function() {
