4b22a57c932debc44ff92ffa87731e0d77b48bc6
Simeon Lee <simeonhlee@gmail.com>
2016-10-12 22:21:46 -0700

    Commit out bug-causing import statement

--- client/components/dashboard/activityFeed/BasicInteraction.js
+++ client/components/dashboard/activityFeed/BasicInteraction.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Timeline extends Component {
+export default class BasicInteraction extends Component {
   constructor(props) {
     super(props);
   }
